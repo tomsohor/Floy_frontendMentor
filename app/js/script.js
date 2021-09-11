b42fac0a5c6d23ed8f8deb66bba2067f6a2975cc
Kanya <tomsohor18@kit.edu.kh>
2021-09-11 12:22:17 +0700

    add validation

---
+++ app/js/script.js
@@ -0,0 +1,32 @@
+const email = document.getElementById("email");
+const Form = document.getElementById('form');
+const emailValidation = document.querySelector('.err');
+
+
+
+email.addEventListener('focus',()=>{
+    emailValidation.style.display = "none";
+})
+
+Form.addEventListener('submit',(e)=>{
+    vadateEmail(email.value);
+    e.preventDefault();
+})
+
+function vadateEmail(x){
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(x)){
+        alert('valid');
+
+    }else {
+        if (x == ""){
+            emailValidation.innerHTML = "Empty email";
+            emailValidation.style.display = "block"
+        // alert("email can't be empty invalid");
+    }else{
+        emailValidation.innerHTML = "please enter a valid email address";
+            emailValidation.style.display = "block"
+    }
+    
+}
+}
